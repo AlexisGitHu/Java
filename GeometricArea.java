8b0fa734af1f23f6d27e25038a04516e4413f2c5
AlexisGitHu <AlexisGitHu@users.noreply.github.com>
2021-02-26 12:07:04 +0100

    Eliminando carpeta

--- GeometricArea.java
+++
@@ -1,6 +0,0 @@
-package com.utad.mais.clase4.modelo2;
-
-public interface GeometricArea
-{
-	public Double getArea();
-}
--- modelo2/AreaStrategy.java
+++
@@ -1,6 +0,0 @@
-package com.utad.mais.clase4.modelo2;
-
-public interface AreaStrategy
-{
-	public Double calculaArea(Double parameter);
-}
